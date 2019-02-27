7b66a419e44ec2c510a11ce09af6772bc6d267aa
kzh-dev <dev69.kzh@gmail.com>
2019-02-28 01:34:06 +0900

    Disable Discord notification by default.

--- config.py
+++ config.py
@@ -3,6 +3,6 @@
 LOG_FILENAME = 'log/pine-api-app.log'
 #LOG_FILENAME = ''
 
-DISCORD_URL = 'https://discordapp.com/api/webhooks/550334760573206563/D-Z2_GUux6UgHevUltLlX6TQUETMfzVrWdG_uoIdc2wuGfMQZaCP92uLbnvjx2vSPjbO'
+DISCORD_URL = ''
 DISCORD_NAME = 'PINE Server'
 DISCORD_AVATAR_URL = ''
